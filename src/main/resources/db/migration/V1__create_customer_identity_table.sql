bf315847392f81f2a256b36ddc5c1e80d5416139
keviny123 <keviny123@users.noreply.github.com>
2025-09-28 11:17:54 -0400

    Revert "Merge pull request #1 from keviny123/feat/infra-migrations-ci"
    This reverts commit eb6eb19da2f409bbc4045cc5f7253364de7127d9, reversing
    changes made to d1af4b2cc0df35519af5469f30d4697dd4181267.

--- src/main/resources/db/migration/V1__create_customer_identity_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS customer_identity (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(100),
-  last_name VARCHAR(100),
-  gender VARCHAR(32),
-  dob DATE,
-  ssn VARCHAR(32) UNIQUE
-);
